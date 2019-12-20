026041b0c9e4b9e4b51702be8ff490eb04a7b4a4
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2019-12-20 12:09:51 -0500

    Components rendering

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+import {Card, CardImg, CardTitle, CardBody, CardSubtitle} from 'reactstrap';
+
+export default function CharacterCard(props) {
+  return(
+    <div>
+    <Card key={props.key}>
+      <CardImg src={props.image}/>
+      <CardBody>
+        <CardTitle> {props.name} </CardTitle>
+        <CardSubtitle>  {props.species} </CardSubtitle>
+      </CardBody>
+    </Card>
+    </div>
+
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,44 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+//import CharacterCard from './CharacterCard.js';
+import {Card, CardImg, CardTitle, CardBody, CardSubtitle} from 'reactstrap';
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(function(response){
+        //console.log("Successful response:", response.data.results);
+        setCharacters(response.data.results);
+      })
+      .catch(function(error){
+        console.log("Request error:", error);
+      })
+
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+      <section>
+        {characters.map(function(character, index){
+          return(
+            <Card key={index}>
+              <CardImg src={character.image} />
+              <CardBody>
+                <CardTitle> {character.name} </CardTitle>
+                <CardSubtitle>  {character.species} </CardSubtitle>
+              </CardBody>
+            </Card>
+          )
+        })}
+      </section>
+
+        
   );
 }
+
+
